c4e329f9555664fdcd826462eb89b74bc311604a
Mohamed Omar <momer2856@gmail.com>
2026-09-21 17:11:31 +0300

    Update site URL to Vercel deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://mohamedomar.dev',
+  site: 'https://mohamed-omar-portfolio-livid.vercel.app',
   integrations: [
     tailwind({
       applyBaseStyles: false,
